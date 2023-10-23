80a591360da53b0c05fbebf304556af06733f6af
Rajput, Rajat <Rajat.Rajput@aristocrat.com>
2023-10-23 23:44:11 +0530

    get ipconfig

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
@@ -35,10 +37,20 @@ pub fn App() -> impl IntoView {
 fn HomePage() -> impl IntoView {
     // Creates a reactive value to update the button
     let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
+    let on_clik = move |_| set_count.update(|count| *count += 1);
+
+    let server_act = create_action(|_| get_ipconfig());
 
     view! {
         <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <button on:click=on_clik>"Click Me: " {count}</button>
+        <button on:click=move |_| server_act.dispatch(())>"get_ipconfig: "</button>
+        <span>{server_act.value()}</span>
     }
+}
+
+#[server]
+pub async fn get_ipconfig() -> Result<String, ServerFnError> {
+    let out = Command::new("ipconfig").output().unwrap().stdout;
+    Ok(String::from_utf8(out).unwrap())
 }
